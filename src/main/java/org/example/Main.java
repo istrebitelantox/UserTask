ddc736512b5a7cca86c524616b0e1b984faf3489
istrebitelantox <geekpook@gmail.com>
2022-12-27 14:46:52 +0300

    Stream complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,30 +14,14 @@ public class Main {
         String[] names={"Yura","Dima","Pasha","Vika","Polya","Masha"};
         int[] arr=new Random().ints(1,1001).distinct().limit(1000).toArray();
         List<User> users= new ArrayList<>();
-        List<User> male= new ArrayList<>();
-        List<User> oldMans= new ArrayList<>();
+        List<User> male;
+        List<User> oldMans;
         for(int i=0;i<1000;i++){
             User user=new User(arr[i],names[(int)(Math.random()* names.length)],(int)(Math.random()*100));
-/*            user.setId(arr[i]);
-            user.setName(names[(int)(Math.random()*10)]);
-            user.setAge((int)(Math.random()*100));*/
-            /*users.add("ID : "+user.getId()+" | Name : "+user.getName()+" | Age: "+user.getAge());*/
             users.add(user);
             System.out.println(users.get(i));
         }
-/*        System.out.println(man);
-        System.out.println(older);
-        for(User user:users){
-            if(user.getName().equals("Dima")|user.getName().equals("Yura")|user.getName().equals("Pasha"))
-            {
-                male.add(user);
-            }
-            if(user.getAge()>50){
-                oldMans.add(user);
-            }
-        }
-        System.out.println("Male: "+male+" \n Count of man: "+male.size());
-        System.out.println("Old mans: "+oldMans+" \n Count of oldmans: "+oldMans.size());*/
+
         male=users.stream().filter(x-> (x.toString().contains("Pasha")|x.toString().contains("Yura")|(x.toString().contains("Dima")))==true)
                 .collect(Collectors.toList());
         System.out.println("\nMale: "+male+" \n Count of male: "+male.size());
